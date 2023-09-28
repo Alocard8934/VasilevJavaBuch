c5e8edcd4cc81a0bbc18839e9c8b2286548c08b1
Yuriy Chikhonadskiy <109043213+Alocard8934@users.noreply.github.com>
2023-09-28 22:59:55 +0200

    ShowMeWindowDemo

---
+++ ShowMeWindowDemo.java
@@ -0,0 +1,8 @@
+package VasilevJavaBuch;
+import javax.swing.JOptionPane;
+public class ShowMeWindowDemo {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Erste Software auf Java!");
+    }
+    
+}
