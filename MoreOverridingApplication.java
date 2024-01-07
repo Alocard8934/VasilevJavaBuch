b944377354b954853da6398570835059c26b74d0
Yuriy Chikhonadskiy <109043213+Alocard8934@users.noreply.github.com>
2024-01-07 20:41:17 +0100

    MoreOverridingApplication

---
+++ MoreOverridingApplication.java
@@ -0,0 +1,31 @@
+package VasilevJavaBuch;
+
+class Alpha1{
+    String name;
+    void show(){
+        System.out.println("Object " + name);
+    }
+    Alpha1(String name){
+        this.name=name;
+    }
+}
+
+class Bravo1 extends Alpha1{
+    int code;
+    void show(){
+        super.show();
+        System.out.println("Zahlen Fiel " + code);
+    }
+    Bravo1(String name, int code){
+        super(name);
+        this.code = code;
+    }
+}
+public class MoreOverridingApplication {
+    public static void main(String[] args) {
+        Alpha1 objA = new Alpha1("objA");
+        Bravo1 objB = new Bravo1("objB", 123);
+        objA.show();
+        objB.show();
+    }
+}
