a756412fd159c1e8240e3a84314cb619a543fce3
Yuriy Chikhonadskiy <109043213+Alocard8934@users.noreply.github.com>
2024-01-07 20:40:32 +0100

    UsingPackageApplication

---
+++ UsingPackageApplication.java
@@ -0,0 +1,10 @@
+package VasilevJavaBuch;
+import static javax.swing.JOptionPane.*;
+public class UsingPackageApplication {
+    public static void main(String[] args) {
+        showMessageDialog(null, 
+                "Static import ist bequem!",
+                "Packet MyPack",
+                WARNING_MESSAGE);
+    }
+}
